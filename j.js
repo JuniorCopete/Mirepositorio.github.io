3f128f74d430664cf07bd60bcc8902c56bec0153
JuniorCopete <107282444+JuniorCopete@users.noreply.github.com>
2022-09-20 13:48:58 -0500

    Mi repositorio aun faltan proyectos
    Faltan otros proyectos en el link de mis proyectos

---
+++ j.js
@@ -0,0 +1,7 @@
+swal.fire("Bienvenid@ a mi portafolio", "Espero tengas una buena experiencia");function hola(){swal.fire("Lo siento!", "No hay nada disponible por ahora", "info");}
+function lista(){
+    Swal.fire({
+        title: '<b>Mis proyectos web</b><br> <a href="https://quibdotur1.000webhostapp.com/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/994/994649.png" alt="proyecto sitio web de ventas" width="30%"></a><h3>Sitio web de ventas</h3>           <a href=""><img src="" alt="" width="30%"></a><h3></h3>',
+        footer: ''
+      })
+}
